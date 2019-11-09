e242735259de12743e35eec5d3c49bfbb214039a
doorBW <doorbw@outlook.com>
2019-11-09 22:26:13 +0900

    model update

--- T_MarketPlace/main/models.py
+++ T_MarketPlace/main/models.py
@@ -4,15 +4,21 @@ from django.utils import timezone
 # Create your models here.
 
 
+
 class Market(models.Model):
     user = models.CharField(max_length=20)
     photo = models.ImageField(upload_to='images/')
-    content = models.TextField()
+    url = models.TextField(null=True)
+    content = models.TextField(null=True)
+    latitude = models.CharField(max_length=100)
+    longitude = models.CharField(max_length=100)
     created_at = models.DateTimeField(default=timezone.now)
 
 
 class Festival(models.Model):
+    market = models.ForeignKey(Market, on_delete = models.CASCADE)
     name = models.CharField(max_length=20)
     photo = models.ImageField(upload_to='images/')
-    content = models.TextField()
+    url = models.TextField(null=True)
+    content = models.TextField(null=True)
     created_at = models.DateTimeField(default=timezone.now)
