13b788f3714c9eadf4d4c0166734ac48a6ce2e01
doorBW <doorbw@outlook.com>
2019-11-09 23:51:41 +0900

    model update 3"
    ;

--- T_MarketPlace/main/forms.py
+++ T_MarketPlace/main/forms.py
@@ -5,10 +5,10 @@ from .models import *
 class MarketForm(forms.ModelForm):
     class Meta:
         model = Market
-        fields = ['user', 'photo', 'content', ]
+        fields = ['author', 'photo', 'content', ]
 
         help_text = {
-            'user': '작성자',
+            'author': '작성자',
             'photo': '사진 업로드',
             'content': '간단한 설명'
         }
--- T_MarketPlace/main/models.py
+++ T_MarketPlace/main/models.py
@@ -3,8 +3,6 @@ from django.utils import timezone
 
 # Create your models here.
 
-
-
 class Market(models.Model):
     author = models.CharField(max_length=20)
     name = models.CharField(max_length=100)
