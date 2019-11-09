ee2c5f0b218e7d0387ed96faaa6d98161ee1601e
doorBW <doorbw@outlook.com>
2019-11-09 17:55:13 +0900

    project setting

--- T_MarketPlace/T_MarketPlace/urls.py
+++ T_MarketPlace/T_MarketPlace/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import main.views
 
 urlpatterns = [
+    path('',  main.views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
